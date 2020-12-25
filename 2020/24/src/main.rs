b4e1ad4ea9f15de7653a3a07f4cd4ce97101a761
Olaf Flebbe <of@oflebbe.de>
2020-12-25 10:12:30 +0100

    twentyfour

--- 2020/24/src/main.rs
+++ 2020/24/src/main.rs
@@ -27,28 +27,98 @@ fn handle( line: &str) -> (i32, i32) {
     (posx, posy)
 }
 
-fn problem( input : &str) -> u32 {
+fn problem( input : &str) ->  HashMap<(i32,i32),bool> {
     let lines : Vec<&str> = input.lines().collect();
-    let mut counter = HashMap::new();
+    let mut tiles = HashMap::new();
     for line in lines {
-        
             let (x, y) = handle( line );
-            let c = counter.entry((x,y)).or_insert(0);
-            *c+=1;
+            let c = tiles.entry((x,y)).or_insert(false);
+            // flip
+            *c = !*c;
     }
-    let mut black = 0;
-    for (k, v) in counter {
-        if v % 2 == 1 {
-            black +=1;
+    tiles
+}
+
+
+
+// true == black
+fn day( tiles : &HashMap<(i32,i32),bool>) -> HashMap<(i32,i32),bool>    {
+
+    let maxx = tiles.iter().map( |(&key,_)| key.0).max().unwrap()+1;
+    let maxy = tiles.iter().map( |(&key,_)| key.1).max().unwrap()+1;
+    let minx = tiles.iter().map( |(&key,_)| key.0).min().unwrap()-1;
+    let miny = tiles.iter().map( |(&key,_)| key.1).min().unwrap()-1;
+   
+    let mut newDay : HashMap<(i32,i32),bool> = HashMap::new();
+
+    for x in minx..=maxx {
+        for y in miny..=maxy {
+            let mut blacks = 0;
+            let coord = (x,y);
+            for d in 0..6 {
+                let (dx, dy) = match d {
+                    0 => ( 1, 0),
+                    1 => ( -1, 0),
+                    2 => ( 0, 1),
+                    3 => ( 0, -1),
+                    4 => (-1, 1),
+                    5 => ( 1, -1),
+                    _ => panic!("Problem") 
+                };
+                let v = (x + dx, y+dy);
+                blacks += match tiles.get(&v) {
+                    Some(&true) => 1,
+                    _ => 0,
+                }
+            }
+            let c = newDay.entry(coord).or_insert(false);
+            *c = match tiles.get(&coord) {
+                Some(&true) => match blacks {
+                        0|3|4|5|6 => false,
+                        _ => true,
+                    },
+                Some(&false)| None => match blacks {
+                    2 => true,
+                    _ => false,
+                },
+            }
         }
     }
-    black
+    newDay
 }
+
+fn counter( tiles : &HashMap<(i32,i32),bool>) -> u32 {
+    let mut count = 0;
+    for (_, v) in tiles {
+        if v == &true {
+            count+=1;
+        }
+    }
+    count
+}
+
 
 fn main() {
     let input = include_str!("test.txt");
-    print!("{}\n", problem( input));
+    let mut test = problem( input);
+    let black = counter(&test);
+    
+    assert_eq!(black ,10);
+
+    for i in 0..100 {
+        test = day( &test);
+    }
+    let black = counter(&test);
+    assert_eq!(black ,2208);
 
     let input = include_str!("input.txt");
-    print!("{}\n", problem( input));
+    let mut tiles = problem( input);
+  
+    print!("{}\n",black);
+ 
+    for i in 0..100 {
+        tiles =  day( &tiles);
+       
+    }
+    println!("{}",counter(&tiles));
 }
